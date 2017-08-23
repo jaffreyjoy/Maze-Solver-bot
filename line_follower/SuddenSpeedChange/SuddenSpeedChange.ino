8e20d3d80c07a2b43f42c2a5ea7582f8d19ebb80
AkashG6 <akashgarule@gmail.com>
2017-08-23 22:00:21 +0530

    addded budget spreadsheet

---
+++ line_follower/SuddenSpeedChange/SuddenSpeedChange.ino
@@ -0,0 +1,92 @@
+#define Lp 7 //left pin of positive motor
+#define Ln 4 // left negative
+#define El 5 // Enable pin of left motor
+#define Rp 8 //right motor positive pin
+#define Rn 12 //pin no of right negative motor
+#define Er 6 //Enable pin of right motor. if it is 0 right motor is always off
+#define led 13
+#define obsSensor 2
+
+int i=0;
+
+void setup() 
+{
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(obsSensor,INPUT);
+  pinMode(Lp,OUTPUT);
+  pinMode(Ln,OUTPUT);
+  pinMode(El,OUTPUT);
+  pinMode(Er,OUTPUT);
+  pinMode(Rp,OUTPUT);
+  pinMode(Rn,OUTPUT);
+  pinMode(led,OUTPUT);
+}
+
+void loop() 
+{
+  
+  MotorControl(1,1);
+  
+  if(i>=100)
+  {
+    digitalWrite(El,HIGH);
+    digitalWrite(Er,HIGH);
+  }
+  
+  else
+  {
+    digitalWrite(El,HIGH);
+    digitalWrite(Er,HIGH);
+    delay(10);
+  
+    digitalWrite(El,LOW);
+    digitalWrite(Er,LOW);
+    delay(40);
+  }
+  
+  i++;
+  Serial.println(i);
+}
+
+void MotorControl(int driveL,int driveR)
+{
+  switch(driveL)
+  {
+    case 0: 
+      digitalWrite(Ln,LOW);
+      digitalWrite(Lp,LOW);
+      break;
+    case 1:
+      digitalWrite(Ln,HIGH);
+      digitalWrite(Lp,LOW);
+      break;
+    case 2:
+      digitalWrite(Ln,LOW);
+      digitalWrite(Lp,HIGH);
+      break;
+    default: 
+      break;
+  }
+  
+  switch(driveR)
+  {
+    case 0:
+      digitalWrite(Rn,LOW);
+      digitalWrite(Rp,LOW);
+      break;
+    case 1:
+      digitalWrite(Rn,HIGH);
+      digitalWrite(Rp,LOW);
+      break;
+    case 2:
+      digitalWrite(Rn,LOW);
+      digitalWrite(Rp,HIGH);
+      break;
+    default: 
+      break;
+  }
+  
+}
+
+
